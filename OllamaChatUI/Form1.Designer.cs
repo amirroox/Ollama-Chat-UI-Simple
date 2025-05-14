54228d5ca6a473f41ed1d15a8dc468e60aefc06f
Amir Roox <amirroox@yahoo.com>
2025-05-14 17:50:00 +0330

    Initial Commit

--- OllamaChatUI/Form1.Designer.cs
+++ OllamaChatUI/Form1.Designer.cs
@@ -28,13 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtUserInput = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtResponse = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // txtUserInput
+            // 
+            this.txtUserInput.AccessibleName = "";
+            this.txtUserInput.Location = new System.Drawing.Point(12, 12);
+            this.txtUserInput.Name = "txtUserInput";
+            this.txtUserInput.Size = new System.Drawing.Size(380, 20);
+            this.txtUserInput.TabIndex = 0;
+            // 
+            // btnSend
+            // 
+            this.btnSend.AccessibleName = "";
+            this.btnSend.Location = new System.Drawing.Point(410, 12);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 1;
+            this.btnSend.Text = "Enter";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtResponse
+            // 
+            this.txtResponse.AccessibleName = "";
+            this.txtResponse.Location = new System.Drawing.Point(12, 38);
+            this.txtResponse.Name = "txtResponse";
+            this.txtResponse.Size = new System.Drawing.Size(776, 387);
+            this.txtResponse.TabIndex = 3;
+            this.txtResponse.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtResponse);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtUserInput);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtUserInput;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.RichTextBox txtResponse;
     }
 }
 
--- OllamaChatUI/Form1.cs
+++ OllamaChatUI/Form1.cs
@@ -1,20 +1,55 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
+using System.Net.Http;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
 
 namespace OllamaChatUI
 {
     public partial class Form1: Form
     {
+        private readonly HttpClient httpClient = new HttpClient();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            string userMessage = txtUserInput.Text.Trim();
+            if (string.IsNullOrEmpty(userMessage)) return;
+
+            txtResponse.Text += $"👤 شما: {userMessage}\n";
+
+            var requestBody = new
+            {
+                model = "gemma3:1b",
+                stream = false,
+                messages = new[]
+                {
+                    new { role = "user", content = userMessage }
+                }
+            };
+
+            var content = new StringContent(JsonConvert.SerializeObject(requestBody), Encoding.UTF8, "application/json");
+
+            try
+            {
+                var response = await httpClient.PostAsync("http://localhost:11434/api/chat", content);
+                var responseBody = await response.Content.ReadAsStringAsync();
+
+                dynamic json = JsonConvert.DeserializeObject(responseBody);
+                string responseText = json.message.content;
+
+                txtResponse.Text += $"🤖 Ollama: {responseText}\n\n";
+                txtUserInput.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("خطا در ارتباط با Ollama: " + ex.Message);
+            }
         }
     }
 }
